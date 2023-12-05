1bb762403c29e55ce62be1fc6261f82d07c4b828
Nobutora Iyoda <nobysemail@gmail.com>
2023-12-04 19:07:35 -0500

    Add cs50 capstone projects

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -17,6 +17,24 @@ export const projects = [
   //   code: 'https://google.com',
   //   id: 1,
   // },
+  // {
+  //   title: 'School Portal',
+  //   description: "Capstone Project done for Harvard CS50's Web Programming with Python and JavaScript. A Django application that changes function based on the user logged in.",
+  //   image: '',
+  //   tags: ['Django', 'SQLite'],
+  //   source: '',
+  //   code: 'https://github.com/me50/Noby1Kenobi/tree/web50/projects/2020/x/capstone',
+  //   id: 2,
+  // },
+  // {
+  //   title: 'Spelunker',
+  //   description: "A top down arcade game where the player explores procedurally generated levels, collects coins, and fights off monsters. A Capstone Project Capstone Project done for Harvard CS50's Introduction to Game Development",
+  //   image: '',
+  //   tags: ['Unity'],
+  //   source: '',
+  //   code: 'https://github.com/Noby1Kenobi/CatMiners/tree/demo',
+  //   id: 3,
+  // },
   {
     title: '2D Fighting Game',
     description: "A simple two player fighting game that uses JavaScript to manage game state.",
@@ -24,7 +42,7 @@ export const projects = [
       tags: ['Html', 'CSS', 'JavaScript'],
     source: 'https://nobygames-fighting-game.netlify.app/',
     code: 'https://github.com/Noby1Kenobi/fighting-game',
-    id: 2,
+    id: 4,
   },
   {
     title: 'Fantasty Pong',
@@ -33,7 +51,7 @@ export const projects = [
     tags: ['Unity'],
     source: 'https://healing-digital.itch.io/fantasy-pong',
     code: 'https://github.com/Noby1Kenobi/PongGame',
-    id: 3,
+    id: 5,
   },
 ];
 
